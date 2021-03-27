82434e83cfb71d9b408a3e2fa952f98aff1bcd01
Philip Wizenty <pwizenty@gmail.com>
2021-03-27 10:23:20 +0100

    Initial commit

---
+++ insurance_company_case_study/README.md
@@ -0,0 +1,24 @@
+# LEMMA Insurance Company Example
+
+This modeling example for the [Language Ecosystem for Modeling Microservice Architecture (LEMMA)](https://github.com/SeelabFhdo/lemma/) contains models that reconstruct the software architecture of [Lakeside Mutual](https://github.com/Microservice-API-Patterns/LakesideMutual). Lakeside Mutual is an example application for [Microservice API Patterns (MAP)](https://microservice-api-patterns.org/) and other patterns (DDD, PoEAA, EIP).
+
+The following components of Lakeside Mutual were reconstructed leveraging LEMMA:
+- `customer-core`
+- `customer-management-backend`
+- `customer-self-service-backend`
+- `eureka-server`
+- `policy-management-backend`
+- `spring-boot-admin`
+
+Moreover, technology models were created with LEMMA's Technology Modeling Language [3] for the following technologies being employed by Lakeside Mutual:
+- [ActiveMQ](https://activemq.apache.org/)
+- [Docker](https://www.docker.com/)
+- [Embedded Architectural Decision Records](https://adr.github.io/e-adr/)
+- [Eureka](https://www.github.com/Netflix/eureka)
+- [Hystrix](https://github.com/Netflix/Hystrix)
+- [Java/Spring](https://www.spring.io)
+- [Microservice API Patterns](https://microservice-api-patterns.org/)
+
+## References
+- Rademacher, Florian, Sabine Sachweh, and Albert Zündorf. ["A Modeling Method for Systematic Architecture Reconstruction of Microservice-Based Software Systems."](https://link.springer.com/chapter/10.1007/978-3-030-49418-6_21) Enterprise, Business-Process and Information Systems Modeling. Springer, Cham, 2020. 311-326.
+- Rademacher, Florian, Sabine Sachweh, and Albert Zündorf. ["Aspect-oriented modeling of technology heterogeneity in microservice architecture."](https://ieeexplore.ieee.org/abstract/document/8703913) 2019 IEEE International Conference on Software Architecture (ICSA). IEEE, 2019.
---
+++ run_container_base_generators.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+# This script runs the code generators for creating all deployment relevent artifacts, e.g.,
+# Dockerfiles, Kubernetes deployment files and build scripts.
+# The script requires the file path to the LEMMA folder as a command line parameter. 
+
+if [ ! -z "$1" ]; then
+  LEMMA_PATH="$1"
+fi
+
+SCRIPT_PATH="scripts/run_model_processor.py"
+CONTAINER_BASE_PATH="code generators/de.fhdo.lemma.model_processing.code_generation.container_base/docker/execution.yaml"
+
+mkdir operation_model_codegeneration
+
+
+####################################################################################################
+# Operation Model Code Generation
+####################################################################################################
+
+# Container Base Generator - Customer Core Service
+"$LEMMA_PATH/$SCRIPT_PATH" \
+    "$LEMMA_PATH/$CONTAINER_BASE_PATH" \
+    -b "$PWD" \
+         -s "insurance_company_case_study/LEMMA models/customer-core/customerCore.operation" \
+         -i "insurance_company_case_study/intermediate/operation models/customerCore.xmi" \
+    -t "$PWD/operation_model_codegeneration"
+
+# Container Base Generator - Customer Management Backend    
+"$LEMMA_PATH/$SCRIPT_PATH" \
+    "$LEMMA_PATH/$CONTAINER_BASE_PATH" \
+    -b "$PWD" \
+         -s "insurance_company_case_study/LEMMA models/customer-management-backend/customerManagementBackend.operation" \
+         -i "insurance_company_case_study/intermediate/operation models/customerManagementBackend.xmi" \
+    -t "$PWD/operation_model_codegeneration"
+
+# Container Base Generator - Self Service    
+"$LEMMA_PATH/$SCRIPT_PATH" \
+    "$LEMMA_PATH/$CONTAINER_BASE_PATH" \
+    -b "$PWD" \
+         -s "insurance_company_case_study/LEMMA models/customer-self-service-backend/customerSelfServiceBackend.operation" \
+         -i "insurance_company_case_study/intermediate/operation models/customerSelfServiceBackend.xmi" \
+    -t "$PWD/operation_model_codegeneration"
+    
+# Container Base Generator - Policy Management    
+"$LEMMA_PATH/$SCRIPT_PATH" \
+    "$LEMMA_PATH/$CONTAINER_BASE_PATH" \
+    -b "$PWD" \
+         -s "insurance_company_case_study/LEMMA models/policy-management-backend/policyManagementBackend.operation" \
+         -i "insurance_company_case_study/intermediate/operation models/policyManagementBackend.xmi" \
+    -t "$PWD/operation_model_codegeneration"
